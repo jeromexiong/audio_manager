5ac7c8e4a726d59895d2b16cb77f457d7a097d31
Jerome Xiong <1540428743@qq.com>
2020-01-20 14:40:41 +0800

    update demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,36 +13,78 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  bool isPlaying = false;
+  int _duration;
+  int _position;
+  num _slider;
 
   @override
   void initState() {
     super.initState();
+
     initPlatformState();
+
+    setupAudio();
+  }
+
+  @override
+  void dispose() {
+    AudioManager.instance.stop();
+    super.dispose();
+  }
+
+  void setupAudio() {
+    AudioManager.instance
+        .start(
+            // "assets/audio.mp3",
+            "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+            "title",
+            desc: "desc",
+            // cover: "assets/ic_launcher.png",
+            cover:
+                "https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png")
+        .then((err) {
+      print(err);
+    });
+
     AudioManager.instance.onEvents((events, args) {
-      print("$events, $args");
+      // print("$events, $args");
       switch (events) {
         case AudioManagerEvents.buffering:
-          print("buffering");
+          print("buffering $args");
           break;
         case AudioManagerEvents.playstatus:
-          print("playstatus");
+          isPlaying = AudioManager.instance.isPlaying;
+          setState(() {});
           break;
         case AudioManagerEvents.timeupdate:
+          _duration = AudioManager.instance.duration;
+          _position = AudioManager.instance.position;
+          _slider = _position / _duration;
+          setState(() {});
           AudioManager.instance.updateLrc(args["position"].toString());
           break;
         case AudioManagerEvents.error:
           print("error");
           break;
         case AudioManagerEvents.next:
-          print("next");
+          next();
           break;
         case AudioManagerEvents.previous:
-          print("previous");
+          previous();
           break;
         default:
           break;
       }
     });
+  }
+
+  void next() {
+    print("next audio");
+  }
+
+  void previous() {
+    print("previous audio");
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -54,10 +96,6 @@ class _MyAppState extends State<MyApp> {
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
@@ -70,53 +108,112 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Plugin audio player'),
         ),
         body: Center(
           child: Column(
             children: <Widget>[
               Text('Running on: $_platformVersion\n'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.play_circle_outline),
-                    onPressed: () {
-                      AudioManager.instance
-                          .start(
-                              // "assets/audio.mp3",
-                              "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-                              "title",
-                              desc: "desc",
-                              // cover: "assets/ic_launcher.png",
-                              cover:
-                                  "https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png")
-                          .then((err) {
-                        print(err);
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.pause_circle_outline),
-                    onPressed: () async {
-                      bool isPlaying =
-                          await AudioManager.instance.playOrPause();
-                      print("await -- $isPlaying");
-                      print("instance -- ${AudioManager.instance.isPlaying}");
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.stop),
-                    onPressed: () {
-                      AudioManager.instance.stop();
-                    },
-                  )
-                ],
-              )
+              Expanded(child: Center(child: Text("lrc text: $_position"))),
+              bottomPanel()
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget bottomPanel() {
+    return Column(children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            IconButton(
+              onPressed: () => previous(),
+              icon: Icon(
+                Icons.skip_previous,
+                size: 32.0,
+                color: Colors.black,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                bool isPlaying = await AudioManager.instance.playOrPause();
+                print("await -- $isPlaying");
+              },
+              padding: const EdgeInsets.all(0.0),
+              icon: Icon(
+                isPlaying ? Icons.pause : Icons.play_arrow,
+                size: 48.0,
+                color: Colors.black,
+              ),
+            ),
+            IconButton(
+              onPressed: () => next(),
+              icon: Icon(
+                Icons.skip_next,
+                size: 32.0,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+      Slider(
+        value: _slider ?? 0.0,
+        activeColor: Colors.red,
+        onChanged: (value) {
+          setState(() {
+            _slider = value;
+          });
+        },
+        onChangeEnd: (value) {
+          if (_duration != null) {
+            int msec = (_duration * value).round();
+            AudioManager.instance.seekTo(msec);
+          }
+        },
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 8.0,
+        ),
+        child: _timer(context),
+      ),
+    ]);
+  }
+
+  Widget _timer(BuildContext context) {
+    var style = TextStyle(color: Colors.black);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Text(
+          _formatDuration(_position),
+          style: style,
+        ),
+        Text(
+          _formatDuration(_duration),
+          style: style,
+        ),
+      ],
+    );
+  }
+
+  String _formatDuration(int msec) {
+    if (msec == null) return "--:--";
+    Duration d = Duration(milliseconds: msec);
+    int minute = d.inMinutes;
+    int second = (d.inSeconds > 60) ? (d.inSeconds % 60) : d.inSeconds;
+    String format = ((minute < 10) ? "0$minute" : "$minute") +
+        ":" +
+        ((second < 10) ? "0$second" : "$second");
+    return format;
   }
 }
--- lib/audio_manager.dart
+++ lib/audio_manager.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
+/// 播放回调事件枚举
 enum AudioManagerEvents {
   buffering,
   playstatus,
@@ -12,6 +12,10 @@ enum AudioManagerEvents {
   unknow
 }
 typedef void Events(AudioManagerEvents events, args);
+
+/// 播放速率枚举 [0.5, 0.75, 1, 1.5, 1.75, 2]
+enum AudioManagerRate { rate50, rate75, rate100, rate150, rate175, rate200 }
+const _rates = [0.5, 0.75, 1, 1.5, 1.75, 2];
 
 class AudioManager {
   static AudioManager _instance;
@@ -29,9 +33,23 @@ class AudioManager {
       ..setMethodCallHandler(_handler);
   }
 
-  bool _playing = false;
   /// 当前播放状态
   bool get isPlaying => _playing;
+  bool _playing = false;
+  void _setPlaying(bool playing) {
+    _playing = playing;
+    if (_events != null) {
+      _events(AudioManagerEvents.playstatus, _playing);
+    }
+  }
+
+  /// 当前播放时长（毫秒
+  int get position => _position;
+  int _position = 0;
+
+  /// 当前播放总时长（毫秒
+  int get duration => _duration;
+  int _duration = 0;
 
   Future<dynamic> _handler(MethodCall methodCall) {
     switch (methodCall.method) {
@@ -40,15 +58,23 @@ class AudioManager {
           _events(AudioManagerEvents.buffering, methodCall.arguments);
         break;
       case "playstatus":
-        _playing = methodCall.arguments;
-        if (_events != null)
-          _events(AudioManagerEvents.playstatus, methodCall.arguments);
+        _setPlaying(methodCall.arguments);
         break;
       case "timeupdate":
+        _position = methodCall.arguments["position"];
+        _duration = methodCall.arguments["duration"];
+        if (!_playing) _setPlaying(true);
+        if (_position < 0 || _duration < 0) break;
+        if (_position > _duration) {
+          _position = _duration;
+          _setPlaying(false);
+        }
         if (_events != null)
-          _events(AudioManagerEvents.timeupdate, methodCall.arguments);
+          _events(AudioManagerEvents.timeupdate,
+              {"position": _position, "duration": _duration});
         break;
       case "error":
+        if (_playing) _setPlaying(false);
         if (_events != null)
           _events(AudioManagerEvents.error, methodCall.arguments);
         break;
@@ -103,6 +129,19 @@ class AudioManager {
   Future<bool> playOrPause() async {
     bool result = await _channel.invokeMethod("playOrPause");
     return result;
+  }
+
+  /// `position` 移动位置 毫秒时间戳
+  Future<String> seekTo(int position) async {
+    if (position < 0 || position > duration)
+      throw "[position] must be greater than 0 and less than the total duration";
+    return await _channel.invokeMethod("seekTo", {"position": position});
+  }
+
+  /// `rate` 播放速率，默认 1.0
+  Future<String> setSpeed(AudioManagerRate rate) async {
+    int _rate = _rates[rate.index];
+    return await _channel.invokeMethod("seekTo", {"rate": _rate});
   }
 
   /// 停止播放
