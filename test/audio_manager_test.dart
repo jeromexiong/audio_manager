f0e41113cf930f852d14dda59e13320902ccf1e3
jerome <19979929623@163.com>
2026-08-14 12:14:10 +0800

    chore: migrate package and example to Dart 3

--- test/audio_manager_test.dart
+++ test/audio_manager_test.dart
@@ -6,15 +6,18 @@ void main() {
   const MethodChannel channel = MethodChannel('audio_manager');
 
   TestWidgetsFlutterBinding.ensureInitialized();
+  final messenger = TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger;
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+    messenger.setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+      if (methodCall.method == 'getPlatformVersion') return '42';
+      if (methodCall.method == 'currentVolume') return 0.5;
+      return null;
     });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    messenger.setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
